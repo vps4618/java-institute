eb654bd98c892e8d0d3e88bfd96f36f85210d630
vishwa praveen <vishwapraveen4618@gmail.com>
2022-10-25 23:48:35 +0530

    game first step created

---
+++ java/OOPC_Test_Day_003/src/Main/A.java
@@ -0,0 +1,18 @@
+package Main;
+
+class A {
+
+    public static void name() {
+        System.out.println("Vishwa Praveen");
+        city();
+    }
+
+    public static void city() {
+        System.out.println("Ingiriya");
+    }
+
+    public static void main(String[] args) {
+        name();
+    }
+
+}
---
+++ java/OOPC_Test_Day_003/src/Main/Main_Class.java
@@ -0,0 +1,13 @@
+package Main;
+
+// shortcuts
+// 1.print - sout
+// 2.create main class - psvm
+
+public class Main_Class {
+
+    public static void main(String[] args) {
+        System.out.println("Main machine running !!");
+    }
+
+}
